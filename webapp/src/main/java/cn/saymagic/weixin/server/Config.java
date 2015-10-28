0aa0c3d9b4f672c25e625111d6e3fa2dc1a95a00
hyhyxu <hyhyxu@gmail.com>
2015-10-28 10:16:49 +0800

    add apikey

--- webapp/src/main/java/cn/saymagic/weixin/server/Config.java
+++ webapp/src/main/java/cn/saymagic/weixin/server/Config.java
@@ -4,8 +4,8 @@ public class Config {
 
 	/** 微信公众号的token，用于验证之用*/
 	public static final String TOKEN = "";
-	
+
 	/** 图灵机器人的APPKEY，可在图灵官方申请*/
-	public static final String APIKEY = ""; 
-	
+	public static final String APIKEY = "aad1050fb62973c9e1ac705a6be8ceb2";
+
 }
